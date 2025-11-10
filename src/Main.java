a3120a974a45eca3dfe1e6f386eda162444cb670
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-11-10 07:36:50 -0300

    Inicializando o array

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Inicializa com 0, false, null
+        int numeros[] = new int[10];
+        String nomes[] = new String[3];
+        boolean testes[] = new boolean[5];
+        int numeros2[] = {2,6,1,0,4,7};
+
+    }
+
+}

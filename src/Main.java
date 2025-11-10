afa3bfddd653220fe316feecdacd3b98dac13614
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-11-10 07:51:51 -0300

    Exemplo de percurso no array

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,20 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //Inicializa com 0, false, null
-        int numeros[] = new int[10];
-        String nomes[] = new String[3];
-        boolean testes[] = new boolean[5];
-        int numeros2[] = {2,6,1,0,4,7};
+        int numeros[] = {2,6,1,0,4,7};
+
+        for(int i=0; i<numeros.length;i++){
+            numeros[i]++;
+        }
+
+        //Não reflete no array
+        for(int i : numeros){
+            i++;
+        }
+
+        for(int i: numeros){
+            System.out.println(i);
+        }
 
     }
 

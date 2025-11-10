ce56256767b4cb5ab9bc17ae0eea10de3995c23a
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-11-10 08:33:27 -0300

    Criando exemplo com produtos

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,14 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        int matriz[][] = new int[3][];
-        matriz[0] = new int[6];
-        matriz[1] = new int[2];
-        matriz[2] = new int[4];
-
-        for(int i=0;i<matriz.length;i++){
-            for(int j=0;j<matriz[i].length;j++){
-                System.out.print(matriz[i][j]+" ");
-            }
-            System.out.println();
-        }
-
-
+        Produto produtos[] = {
+                new Produto(2, "Arroz", 4.5f),
+                new Produto(1, "Feijão", 8.6f),
+                new Produto(3, "Macarrão", 3.8f)
+        };
+        System.out.println(Arrays.toString(produtos));
     }
 }
